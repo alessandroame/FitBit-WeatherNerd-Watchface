1863a4fbf1328c9342247d458ba4b315f57dc6bd
aame <alessandroame@gmail.com>
2020-10-04 14:05:35 +0200

    mediator log cleanup

--- common/mediator.js
+++ common/mediator.js
@@ -4,17 +4,17 @@ let intanceID=new Date().getUTCMilliseconds();
 export function subscribe(topic,callback){
   if (!subscriptions[topic]) subscriptions[topic]=[];
   subscriptions[topic].push(callback);
-  console.log("Mediator ["+intanceID+"] subscriptions "+subscriptions.length);
+  console.log("Mediator #"+intanceID+" subscribe to "+topic);
 }
 
 function notify(evt) {
   var packet=evt.data;
   let callbacks=subscriptions[packet.topic];
   if (callbacks==null || callbacks.length==0) {
-    //console.warn("Mediator ["+intanceID+"] found no subscription for topic: "+packet.topic+"   stack:"+new Error().stack);
+    //console.warn("Mediator #"+intanceID+" found no subscription for topic: "+packet.topic+"   stack:"+new Error().stack);
     return;
   }
-  //console.log("Mediator ["+callbacks.length+"] FOUND subscription for topic: "+packet.topic);
+  //console.log("Mediator #"+intanceID+" ["+callbacks.length+"] FOUND subscription for topic: "+packet.topic);
   callbacks.forEach(
     (callback)=>{
       callback(packet.data);
@@ -22,13 +22,13 @@ function notify(evt) {
 }
 
 export function publish(topic,data) {
-  //console.log("Mediator ["+intanceID+"] publishing topic: "+topic);
+  //console.log("Mediator #"+intanceID+" publishing topic: "+topic);
   let callbacks=subscriptions[topic];
   if (callbacks==null || callbacks.length==0) {
-    //console.warn("Mediator ["+intanceID+"] found no subscription for topic: "+topic+"   stack:"+new Error().stack);
+    //console.warn("Mediator #"+intanceID+" found no subscription for topic: "+topic+"   stack:"+new Error().stack);
     return;
   }
-  //console.log("Mediator ["+intanceID+"] found "+callbacks.length+" callbacks for topic: "+topic);
+  //console.log("Mediator #"+intanceID+" found "+callbacks.length+" callbacks for topic: "+topic);
   callbacks.forEach(
     (callback)=>{
       callback(data);
