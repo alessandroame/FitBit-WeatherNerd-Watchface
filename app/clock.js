c44b2fe87e095e52187d3a7b3b9dd4b7501d92c5
aame <alessandroame@gmail.com>
2020-11-01 11:10:35 +0100

    second hand animation

--- app/clock.js
+++ app/clock.js
@@ -1,3 +1,4 @@
+import { display } from "display";
 import clock from "clock";
 import * as datum from "./datum"
 import document from "document";
@@ -22,11 +23,11 @@ let layer = 98;
 secHand.layer = layer--;
 secHandShadow.layer = layer--;
 
-minHand.layer = layer --;
-minHandShadow.layer = layer --;
+minHand.layer = layer--;
+minHandShadow.layer = layer--;
 
-hourHand.layer = layer --;
-hourHandShadow.layer = layer --;
+hourHand.layer = layer--;
+hourHandShadow.layer = layer--;
 
 
 settings.subscribe("clockBackgroundColor", (color) => {
@@ -72,6 +73,26 @@ export function hide() {
   clockContainer.style.display = "none";
 }
 
+let animated=false;
+let animationTimeoutId = null;
+display.addEventListener("change", () => {
+  animated=true;
+  if (display.on) {
+    stopAnimationTimer();
+    animationTimeoutId = setTimeout(() => {
+      animated=false;
+    }, 5000);
+  } else {
+    stopAnimationTimer()
+  }
+});
+
+function stopAnimationTimer() {
+  if (animationTimeoutId) {
+    clearTimeout(animationTimeoutId);
+    animationTimeoutId = null;
+  }
+}
 function updateClock() {
   let now = new Date();
   let hours = now.getHours() % 12;
@@ -80,9 +101,28 @@ function updateClock() {
 
   hourHand.groupTransform.rotate.angle = geom.hoursToAngle(hours, mins);
   minHand.groupTransform.rotate.angle = geom.minutesToAngle(mins);
-  secHand.groupTransform.rotate.angle = geom.secondsToAngle(secs);
 
   hourHandShadow.groupTransform.rotate.angle = geom.hoursToAngle(hours, mins);
   minHandShadow.groupTransform.rotate.angle = geom.minutesToAngle(mins);
-  secHandShadow.groupTransform.rotate.angle = geom.secondsToAngle(secs);
+  let secAng = geom.secondsToAngle(secs);
+  if (animated){
+    animateSeconds(secHand, secAng);
+    animateSeconds(secHandShadow, secAng);
+  }else{
+    secHand.groupTransform.rotate.angle =secAng;
+    secHandShadow.groupTransform.rotate.angle = geom.secondsToAngle(secs);
+  }
+
+}
+
+function animateSeconds(hand, secAng) {
+  hand.getElementById("t1").from = secAng;
+  hand.getElementById("t1").to = secAng + 2;
+
+  hand.getElementById("t2").from = secAng + 2;
+  hand.getElementById("t2").to = secAng + 4;
+
+  hand.getElementById("t3").from = secAng + 4;
+  hand.getElementById("t3").to = secAng + 6;
+  hand.animate("enable");
 }
