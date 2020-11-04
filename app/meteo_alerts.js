dff8864ec2bd3c79f791559eecb0b488f1188c92
aame <alessandroame@gmail.com>
2020-11-04 21:30:20 +0100

    better alerts scale and opacity

--- app/meteo_alerts.js
+++ app/meteo_alerts.js
@@ -12,65 +12,33 @@ export function update(alerts) {
         let alert = alerts[i];
         let precData = alert.precipitation;
         for (let n = 0; n < 5; n++) {
-            try {
-                let precUI = document.getElementById("p_" + (i * 5 + n));
-                if (precData.probability > 0) {
-                    //console.log(JSON.stringify(precData))
-                    precUI.style.opacity = 0.3 + 0.7 * precData.probability;
-                    precUI.arcWidth = 2 + 14 * precData.quantity;
-                    precUI.style.display = "inline";
-                } else {
-                    precUI.style.display = "none";
-                }
-
-                let ice = document.getElementById("i_" + (i * 5 + n));
-                if (alert.ice.probability > 0) {
-                    ice.style.opacity = alert.ice.probability;
-                    ice.style.display = "inline";
-                } else {
-                    ice.style.display = "none";
-                }
-            } catch (e) {
-                logger.error(e);
-            }
+            updateAlertItem(i*5+n,alert.ice.probability, precData.probability, precData.quantity);
         }
-        // let prec = alertWidgets[i].getElementById("precipitation");
-        // prec.style.opacity = alerts[i].precipitation.probability;
-        // prec.arcWidth = 14 * alerts[i].precipitation.quantity;
-
-        // let ice = alertWidgets[i].getElementById("ice");
-        // ice.style.opacity = alerts[i].ice.probability;
     }
 }
 export function test() {
     console.log("meteo_alerts test");
     for (let i = 0; i < 60; i++) {
         //setTimeout(() => {
-        let prec = document.getElementById("p_" + i);
-        prec.style.opacity = (i + 1) / 60;
-        prec.arcWidth = 15 * (i + 1) / 60;
-        prec.style.display = "inline";
-
-        let ice = document.getElementById("i_" + i);
-        ice.style.opacity = (i + 1) / 60;
-        ice.style.display = "inline";
+            let k=(i + 1) / 60;
+            updateAlertItem(i,k,k,k)
         //}, 30 * i);
     }
 }
 
-function updateAlertUI(index, iceProb, precProb, precQuantity) {
-    let precUI = document.getElementById("p_" + index;
+function updateAlertItem(index, iceProb, precProb, precQuantity) {
+    let precUI = document.getElementById("p_" + index);
     if (precProb > 0) {
         precUI.style.opacity = 0.3 + 0.7 * precProb;
-        precUI.arcWidth = 2 + 14 * precQuantity;
+        precUI.arcWidth = 2 + 18* precQuantity;
         precUI.style.display = "inline";
     } else {
         precUI.style.display = "none";
     }
 
-    let ice = document.getElementById("i_" + (i * 5 + n));
+    let ice = document.getElementById("i_" + index);
     if (iceProb > 0) {
-        ice.style.opacity = iceProb;
+        ice.style.opacity = 0.3 + 0.7 *iceProb;
         ice.style.display = "inline";
     } else {
         ice.style.display = "none";
