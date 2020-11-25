28f2f619179697a2becb082b311e03e45c203beb
aame <alessandroame@gmail.com>
2020-11-25 18:38:18 +0100

    better weather widget

--- app/weather_widget.js
+++ app/weather_widget.js
@@ -1,13 +1,16 @@
 import document from "document";
 
 export function update(meteo){
-    let currentWeather=meteo.forecasts[0];
+    let index=Math.floor(new Date().getHours());
+    if (index>11) index-=12;
+    let currentWeather=meteo.forecasts[index];
 //    console.error(JSON.stringify(currentWeather));
     let container = document.getElementById("currentWeather");
     let icon = container.getElementById("icon");
     icon.href = "icons/meteo/" + currentWeather.icon + ".png";
     let temp = container.getElementById("temp");
     temp.textContent = parseTemp(currentWeather.temp) + currentWeather.tempUnits;
+    container.getElementById("tempShadow").textContent =temp.textContent;
 }
 function parseTemp(v) {
     if (v < 0 && v > -1) v = 0;
